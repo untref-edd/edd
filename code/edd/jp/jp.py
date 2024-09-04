d9bd29983f491bf1074b17cfa6418687d76f64a4
Santiago Rojo <tiagox@gmail.com>
2024-09-04 12:30:06 -0300

    Se agregan mejoras para la clase 5

--- code/edd/jp/jp.py
+++ code/edd/jp/jp.py
@@ -1,4 +1,5 @@
 from base64 import b64encode
+from urllib.parse import urlencode
 
 import networkx as nx  # type: ignore
 from IPython.display import HTML, display
@@ -29,3 +30,31 @@ def render_graphviz_svg(G: nx.Graph) -> None:
     # A.node_attr.update(shape="box", margin=(0.01, 0.1))
     svg = A.draw(format="svg", prog="dot")
     display(HTML(f'<img src="data:image/svg+xml;base64,{b64encode(svg).decode()}">'))
+
+
+def render_python_tutor(code: str) -> None:
+    fragment = {
+        "code": code.strip(),
+        "codeDivHeight": "400",
+        "codeDivWidth": "350",
+        "cumulative": "false",
+        "curInstr": "0",
+        "heapPrimitives": "nevernest",
+        "origin": "opt-rontend.js",
+        "py": "3",
+        "rawInputLstJSON": "[]",
+        "textReferences": "false",
+    }
+
+    display(
+        HTML(
+            f"""
+        <iframe
+            width="100%"
+            height="640"
+            frameborder="0"
+            src="https://pythontutor.com/iframe-embed.html#{urlencode(fragment)}"
+        />
+        """
+        )
+    )
